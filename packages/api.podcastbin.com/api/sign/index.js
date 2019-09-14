5c151b1ce311ee70a06a9d6f05e6529378ecf5da
dfreire <dario.freire@gmail.com>
2019-09-14 15:38:54 +0100

    created sign endpoint

---
+++ packages/api.podcastbin.com/api/sign/index.js
@@ -0,0 +1,8 @@
+module.exports = (req, res) => {
+  res.json({
+    method: "method",
+    url: "url",
+    fields: "fields",
+    headers: "headers"
+  });
+};
--- packages/podcastbin.com/pages/p/example/index.jsx
+++ packages/podcastbin.com/pages/p/example/index.jsx
@@ -1,5 +1,65 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Uppy from "@uppy/core";
+import AwsS3 from "@uppy/aws-s3";
+import { DragDrop } from "@uppy/react";
 
-const Example = () => <h1>Example</h1>;
+const uppy = Uppy({
+  meta: {},
+  restrictions: { maxNumberOfFiles: 1 },
+  autoProceed: true,
+  allowMultipleUploads: false,
+  debug: true
+});
+
+const getUploadParameters = file => {
+  console.log("getUploadParameters");
+  return fetch("http://localhost:3001/api/sign", {
+    method: "post",
+    headers: {
+      accept: "application/json",
+      "content-type": "application/json"
+    },
+    body: JSON.stringify({
+      filename: file.name,
+      contentType: file.type
+    })
+  }).then(response => response.json());
+};
+
+uppy.use(AwsS3, { getUploadParameters });
+
+const Example = () => {
+  useEffect(() => {
+    const onComplete = result => {
+      console.log("onComplete", { result });
+    };
+
+    const onSuccess = (file, data) => {
+      console.log("onComplete", { file, data });
+    };
+
+    uppy.on("complete", onComplete);
+    uppy.on("upload-success", onSuccess);
+
+    return () => {
+      uppy.off("complete", onComplete);
+      uppy.off("upload-success", onSuccess);
+    };
+  });
+
+  return (
+    <div>
+      <DragDrop
+        uppy={uppy}
+        locale={{
+          strings: {
+            dropHereOr: "Drop here or %{browse}",
+            browse: "browse"
+          }
+        }}
+      />
+    </div>
+  );
+};
 
 export default Example;
